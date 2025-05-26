6ffc9fe270985853dcb2552e1deeb37eb564d0f7
Petros Charitopoulos <pcharitopoulos@emagine.com>
2025-05-26 15:16:28 -0400

    Fix static file path for React build in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,11 +30,11 @@ app.use('/api/user', userRoutes);
 
 // Serve static files from the React app
 const path = require('path');
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '..', '..', 'build')));
 
 // The "catchall" handler: for any request that doesn't match above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
 });
 
 // Error handling middleware
