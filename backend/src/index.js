4d7e7f8840cb29116b47eb0273c5c790de375d8b
Petros Charitopoulos <pcharitopoulos@emagine.com>
2025-07-06 20:04:02 -0400

    update route path

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const recipeRoutes = require('./routes/recipe');
 const uploadRoutes = require('./routes/upload');
+const path = require('path');
 
 const app = express();
 
@@ -31,6 +32,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/recipes', recipeRoutes);
 app.use('/api/upload', uploadRoutes);
+
+// Serve static files from the React build directory
+app.use(express.static(path.join(__dirname, '../../build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
